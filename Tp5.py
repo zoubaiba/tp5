d1fe2ee07d85cdf3b4cd699a6185d41aa07f89a2
AIT MOKHTAR Lylia <lylia.ait-mokhtar@etu.univ-amu.fr>
2026-02-17 16:36:57 +0100

    first commit

---
+++ Tp5.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import requests # Pour appeler l'API météo
+
+app = Flask(__name__)
+
+@app.route('/')
+def display_weather():
+    # Récupération de la ville 
+    ville = request.args.get('city', 'MARSEILLE') 
+    
+    API_KEY = "5a5d16a2693b65cf2c2d208670b60f27"
+    url = f"http://api.weatherstack.com/current?access_key={API_KEY}&query={ville}"   
+    r = requests.get(url)
+    data = r.json()
+    print(data)
+    return render_template('meteo.html', data=data)
